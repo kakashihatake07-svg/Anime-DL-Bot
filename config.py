a3feefcff2d7f6fce8b41a9399402042888552c5
Kakashi <76722245+kakashihatake07-svg@users.noreply.github.com>
2021-06-01 15:09:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = "YOUR_TELEGRAM_API_ID"
-api_hash = "YOUR_TELEGRAM_API_HASH"
-bot_token = "YOUR_BOT_TOKEN"
+api_id = "2943069"
+api_hash = "7ba2c6c7a6013f79ef95baab7dba92e3"
+bot_token = "1807849773:AAFZIum1SxeGjkkE0yNmNb5i1HiV0fdErQw"
